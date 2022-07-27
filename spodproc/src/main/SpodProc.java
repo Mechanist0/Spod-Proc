3c01a447645260a3b22a1ab88c1bd354e0a46a80
TheMechanist1 <dminer625@gmail.com>
2022-07-27 12:19:23 -0500

    Add Platform Comparator Rename myComparator to TimeStampComparator

--- spodproc/src/main/SpodProc.java
+++ spodproc/src/main/SpodProc.java
@@ -19,14 +19,23 @@ public class SpodProc {
             endSongObjectArray.add(endSongObject);
         }
 
-        Collections.sort(endSongObjectArray, new MyComparator());
+        Collections.sort(endSongObjectArray, new PlatformComparator());
+        Helper.writeJsonFile(endSongObjectArray, "spodproc/src/resources/endsong_1.json");
     }  
 }
 
 //Compares two EndSongObjects by their timestamps
-class MyComparator implements Comparator<EndSongObject> {
+class TimeStampComparator implements Comparator<EndSongObject> {
     @Override
     public int compare(EndSongObject o1, EndSongObject o2) {
         return o1.getTs().compareTo(o2.getTs());
+    }
+}
+
+//Compares two EndSongObjects by their platform
+class PlatformComparator implements Comparator<EndSongObject> {
+    @Override
+    public int compare(EndSongObject o1, EndSongObject o2) {
+        return Comparator.comparing(EndSongObject::getPlatform).thenComparing(EndSongObject::getTs).compare(o1, o2);
     }
 }
